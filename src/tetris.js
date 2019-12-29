e69a6c8f867a4b1945ec65072cb298710ca6ea3e
michlub665 <mlubecki6@wp.pl>
2019-12-29 13:59:56 +0100

    Update script
    działający skrypt i poprawny index.html
    z 29.12.2019

--- src/tetris.js
+++ src/tetris.js
@@ -1,30 +1,17 @@
-/** JavaScript example
- * 
+/**
+ * http://usejsdoc.org/
  */
 
 var canvas;
 var ctx;
-
-var head;
-var apple;
-var ball;
-
-var dots;
-var apple_x;
-var apple_y;
+var scoreText;
+var score;
 
 var leftDirection = false;
 var rightDirection = true;
 var upDirection = false;
 var downDirection = false;
-var inGame = true;    
-
-//const DOT_SIZE = 10;
-//const ALL_DOTS = 900;
-//const MAX_RAND = 29;
-//const DELAY = 140;
-//const C_HEIGHT = 300;
-//const C_WIDTH = 300;    
+var inGame = true; 
 
 const LEFT_KEY = 37;
 const RIGHT_KEY = 39;
@@ -34,19 +21,20 @@ const DOWN_KEY = 40;
 const partSize=20;
 const startX=60; //starting pos. for test
 const startY=60;
-//var x = new Array(ALL_DOTS);
-//var y = new Array(ALL_DOTS);
-
 
 const C_HEIGHT = 400;	//20points
 const C_WIDTH = 200;  //10 points
+const DELAY = 140;
+//variables for tests
+var testArrayX =[1,1,1,1];
+var testArrayY =[1,2,3,4];
+var gameWindow;
+var testImg;
+var moved =false;
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
 
 class Point{
-	
-	constructor(){
-		this.x=100;
-		this.y=200;
-	}
 	
 	constructor(x,y){
 		this.x=x;
@@ -57,7 +45,7 @@ class Point{
 		this.x=x;
 	}
 	
-	setY(y){
+	setY(y){ 
 		this.y=y;
 	}
 	
@@ -73,67 +61,45 @@ class Point{
 class Block{
 	
 	constructor(){
-//		this.x = new Array(4);
-//		this.y = new Array(4);
 		this.points= new Array();
-		for(var i=0;i<4;i++)
-			points.push(new Point());
+		//this.points= [];
+		for(var i=0;i<4;i++){
+			this.points.push(new Point(i*partSize, i*partSize));
+		}
 		
-		//this.partSize=20;
-	}
-	
-	constructor(arrayX,arrayY){
-//		this.x = new Array(4);
-//		this.y = new Array(4);
-		this.points= new Array();
-		for(var i=0;i<4;i++)
-			points.push(new Point(arrayX[i]*partSize, arrayY[i]*partSize));
-
 	}
 	
 	setPosition(arrayX, arrayY){
-		for(i=0;i<4;i++){
-			x[i]=arrayX[i]*partSize;
-			y[i]=arrayY[i]*partSize;
+		for(var i=0;i<4;i++){
+			this.points[i].setX(arrayX[i]*partSize);
+			this.points[i].setY(arrayY[i]*partSize);
+			ctx.fillText("Blocks"+this.points[i].getX()+" "+this.points[i].getY(), 50, 50+i*20);
 		}
+		ctx.fillText("Game over", 100, 100);
 	}
 
 	getPoints(){
 		return this.points;
 	}
-	
 }
 
-
-/*
- * Game window
- */
 class Window{
-	
-//	var x
-//	var y;
-//	var pole;
-	
+
 	constructor(){
 		this.allPoints = new Array();
-		this.pointIamge = new Image();
+		this.pointImage = new Image();
 		this.currentBlock = new Block();
-		//this.y = new Array(ALL_DOTS);
-//		this.C_HEIGHT = 400;	//20points
-//		this.C_WIDTH = 200;  //10 points
 	}
 	  
 	loadImages() {
 
-		this.pointImage.src = 'red2.png';
-		this.pointImage.width = partSize;
-		this.pointImage.height = partSize;
+		this.pointImage.src = "red2.png";
 	}
 	
 	addBlock(newBlock){
 		this.currentBlock = newBlock;
 		for(var i=0;i<4;i++){
-			this.allPoints.append(newBlock.getPoints[i]);
+			this.allPoints.push(newBlock.getPoints()[i]);
 		}
 	}
 	/*
@@ -142,13 +108,13 @@ class Window{
 	removeRow(index){
 		for(var i=0; i<allPoints.size(); i++){
 			if(allPoints[i].getY() == index*partSize){
-				allPoints.splice(i,1);
+				this.allPoints.splice(i,1);
 			}
 		}
 		
 		for(var i=0; i<allPoints.size(); i++){
 			if(allPoints[i].getY() > index*partSize){
-				allPoints[i].setY(allPoints[i].getY() - partSize)
+				this.allPoints[i].setY(allPoints[i].getY() - partSize)
 			}
 		}
 		
@@ -156,6 +122,20 @@ class Window{
 	
 	move(arrayX,arrayY){
 		this.currentBlock.setPosition(arrayX,arrayY);
+	}
+	
+	//move block by(not TO) x and y
+	moveTestDisplay(x,y){
+		for(var i=0;i<4;i++){
+			testArrayX[i] = testArrayX[i]+x;
+			testArrayY[i] = testArrayY[i]+y;
+		}
+		this.currentBlock.setPosition(testArrayX, testArrayY);
+		//ctx.fillText('Game over', 100,300);
+		
+		score=score+1;
+		document.getElementById("score").innerHTML = "Score: "+score;
+		//scoreText="Score: "+score;
 	}
 	
 	getImage(){
@@ -167,56 +147,72 @@ class Window{
 	}
 	
 	getAllPoints(){
-		return allPoints;
-	}
-	
+		return this.allPoints;
+	}	
 }
+
 
 function init() {
     
-    canvas = document.getElementById('myCanvas');
-    ctx = canvas.getContext('2d');
-    
-    ctx.fillStyle = 'red';
+	//scoreText = document.getElementById("score");
+	document.getElementById("score").innerHTML = "Score: "+0;
+	score=0;
+	
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+     
+    ctx.fillStyle = 'white'; 
     ctx.textBaseline = 'middle'; 
     ctx.textAlign = 'center'; 
     ctx.font = 'normal bold 18px serif';
     
-    ctx.fillText('Game over', C_WIDTH/2, C_HEIGHT/2);
+    testPoint=new Point(100,50);
     
-    let alertText="HALO";
-    alert(alertText);
-    gameOver();
+    testImg = new Image()
+ 
+    testImg.src = "red2.png";
+//  testImg.onload = function(){
+//	  ctx.drawImage(testImg, 0, 0);
+//  }
+    //ctx.drawImage(testImg,60,60,20,20);//(img,x,y,width,height)
     
-//    testImg = new Image()
-//    testImg.src = 'C:\Users\Michał\TrylogiaD\Tetrisred2.png';
-//    testImg.width = 40;
-//    testImg.height = 40;
+    ctx.drawImage(testImg,testPoint.getX(),testPoint.getY(),20,20);//(img,x,y,width,height)
+    //ctx.drawImage(testImg,testPoint.getX()+50,testPoint.getY()+50,20,20);//(img,x,y,width,height)
     
-    //ctx.drawImage(testImg, 100, 100);
     
-//    gameWindow = new Window();
-//    gameWindow.loadImages();
-//    gameWindow.addBlock(new Block())
-//    
-//    setTimeout("gameCycle()", DELAY);
+    
+    
+    
+    
+    gameWindow = new Window();
+    gameWindow.loadImages();
+    gameWindow.addBlock(new Block());
 
-}    
-    
+    gameWindow.getBlock().setPosition(testArrayX,testArrayY);
+    setTimeout("gameCycle()", DELAY);
 
-function doDrawing(window) {
+//    let alertText="HALO";
+//    alert(alertText);
+}
+
+
+
+function doDrawing() {
     
 	
     ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-    
+
     if(inGame){
     	
-    	for(var i=0; i < window.getAllPoints().size(); i++){
-        	ctx.darwImage(pointImg,window.getAllPoints()[i].getX(), window.getAllPoints()[i].getY())
-        }
+    	for(var i=0; i < gameWindow.getAllPoints().length; i++){
+    		ctx.drawImage(gameWindow.getImage(), gameWindow.getAllPoints()[i].getX(), gameWindow.getAllPoints()[i].getY(),20,20);//(img,x,y,width,height)
+        	//ctx.darwImage(pointImg,window.getAllPoints()[i].getX(), window.getAllPoints()[i].getY())
+    		
+    	}
     }else{
     	gameOver();
     }   
+    //ctx.fillText("Game over", 100, 150);
 }
 
 function gameOver() {
@@ -232,50 +228,60 @@ function gameOver() {
 
 
 function gameCycle() {
-    
+	
     if (inGame) {
-
-        
+    	//ctx.fillText("Game over", 100, 300);
         doDrawing();
         setTimeout("gameCycle()", DELAY);
     }
 }
+ 
+/*
+ * Called in function of exception below
+ */
 
-onkeydown = function(e) {
+function send(){
+	
+}
+
+/*
+ * pewnie wolane gameCycle   (nie wiem)
+ */
+function recive(){
+	
+}
+
+
+/*
+ * TODO
+ * zamienic gameWindow.moveTestDisplay(....);
+ * z
+ * send(....)
+ */
+onkeyup = function(e) {
     
     var key = e.keyCode;
     
-    if ((key == LEFT_KEY) && (!rightDirection)) {
-        
-        leftDirection = true;
-        upDirection = false;
-        downDirection = false;
+    if (key == LEFT_KEY) {
+    	
+        gameWindow.moveTestDisplay(-1,0);
     }
 
-    if ((key == RIGHT_KEY) && (!leftDirection)) {
+    if (key == RIGHT_KEY) {
         
-        rightDirection = true;
-        upDirection = false;
-        downDirection = false;
+    	gameWindow.moveTestDisplay(1,0);
     }
 
-    if ((key == UP_KEY) && (!downDirection)) {
+    if (key == UP_KEY) {
         
-        upDirection = true;
-        rightDirection = false;
-        leftDirection = false;
+    	gameWindow.moveTestDisplay(0,-1);
     }
 
-    if ((key == DOWN_KEY) && (!upDirection)) {
+    if (key == DOWN_KEY) {
         
-        downDirection = true;
-        rightDirection = false;
-        leftDirection = false;
+    	gameWindow.moveTestDisplay(0,1);
     }        
 };    
-
-
-
 
 
 
